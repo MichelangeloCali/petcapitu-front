b8f250fdd62dcf5a2089ea3e79bddd1565dc4318
michelangelo <michelangelocali@hotmail.com>
2022-10-11 20:17:47 -0300

    adjustments styles

--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -1,17 +1,16 @@
 import styled from "styled-components"
 
-export const ShowHome = styled.div`
+export const ContentHome = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   gap: 1rem;
-  margin-top: 1rem;
 
   border: none;
   border-radius: 5px;
-  background-color: #fbecfa;;
-  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  background-color: var(--background-content);
+  box-shadow: var(--shadow);
 
-  img {
+  .dogHome {
     display: none;
   }
 
@@ -40,59 +39,28 @@ export const ShowHome = styled.div`
     align-items: center;
 
     h1 {
-      display: block;
       margin-top: 2rem;
       margin-bottom: 1.5rem;
-      text-align: left;
-
-      letter-spacing: .3px;
-      word-spacing: 1px;
-      color: var(--text-title);
-
-    }
-
-    span {
-      text-align: center;
-      color: #F065F0;
-      font-size: 2.5rem;
-
-      @media (min-width: 1080px) {
-        font-size: 2.8rem;
-      }
     }
 
     h3 {
       margin-bottom: 2rem;
+    }
 
-      letter-spacing: .2px;
-      color: var(--text-body);
-
-      @media (min-width: 1080px) {    
+    @media (min-width: 1080px) {
+      button {
+        margin-top: 3rem;
+      }
+      h3 {
         margin-top: 2rem;
         margin-bottom: 3rem;
       }
-    }
-
-    p {
-      text-align: left;
-
-      @media (min-width: 1080px) {    
+      p { 
         width: 33ch;
       }
-    }
-
-    button {
-      margin-top: 2rem;
-      padding: 9px 16px;
-      width: 200px;
-      border: none;
-      border-radius: 5px;
-      background-color: var(--yellow);
-
-      @media (min-width: 1080px) {
-        margin-top: 3rem;
+      span {
+        font-size: 2.8rem;
       }
     }
   }
-
 `
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,10 +4,14 @@ export const GlobalStyle = createGlobalStyle`
 
   :root {
     --body: #FAE9CB;
-    --background-color: #f2f2f2;
+    --background-content: #fbecfa;
     --border: #d7d7d7;
+    --shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+
     --text-title: #4C2270;
     --text-body: #54257D;
+    --text-logo: #F065F0;
+
     --yellow: #f4b359;
     --red: #e52e4d;
     --green: #33cc95;
@@ -44,7 +48,7 @@ export const GlobalStyle = createGlobalStyle`
     font-weight: 400;
   }
 
-  h1, h2, h3, h4, h5, h6, strong {
+  h2, h3, h4, h5, h6, strong {
     font-weight: 600;
   }
 
@@ -60,7 +64,8 @@ export const GlobalStyle = createGlobalStyle`
 
 export const Container = styled.div`
   max-width: 360px;
-  margin-top: 7rem;
+  margin-top: 5rem;
+  margin-bottom: 20rem;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -74,3 +79,41 @@ export const Container = styled.div`
     max-width: 700px;
   }
 `
+
+export const Text1 = styled.h1`
+  display: block;
+  font-weight: 600;
+  color: var(--text-title);
+  letter-spacing: .3px;
+  word-spacing: 1px;
+  text-align: left;
+`
+
+export const Text2 = styled.h3`
+  letter-spacing: .2px;
+  color: var(--text-body);
+`
+
+export const Text3 = styled.p`
+  text-align: left;
+`
+
+export const TextLogo = styled.span`
+  text-align: center;
+  color: var(--text-logo);
+  font-size: 2.5rem;
+`
+
+export const Button = styled.button`
+  margin-top: 2rem;
+  padding: 9px 16px;
+  width: 200px;
+  border: none;
+  border-radius: 5px;
+  background-color: var(--yellow);
+
+  :hover {
+    filter: brightness(0.9);
+  }
+`
+
